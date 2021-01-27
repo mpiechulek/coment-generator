d267535d25659d8680cbbe73f35be230b3741495
MP <47081376+mpiechulek@users.noreply.github.com>
2021-01-27 10:10:28 +0100

    added triggering by enter , and coping to clipboard

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+// =============================================================================
+// ====== This is the best comment, ever written by man, on this planet. =======
+// =============================================================================
+
 const baseString = '// =============================================================================';
 let maxLength = 80;
 let maxCommentLength = 67;
@@ -7,6 +11,7 @@ const clearBtn = document.getElementById('clear-btn');
 const resultParagraph = document.getElementById('result');
 const textArea = document.getElementById('entry-field');
 const copyBtn = document.getElementById('copy-to-clipboard');
+const copyAlert = document.getElementById('coped-succes');
 
 // =============================================================================
 
@@ -68,8 +73,6 @@ const generateFinalCommentString = (commentArr) => {
                 repeat = (maxLength - commentLine.length - 5) / 2;
                 rightSide = symbolRepeat.repeat(repeat);
             }
-            console.log(commentLine.length);
-            console.log(commentLine);
 
             leftSide = symbolRepeat.repeat(repeat);
 
@@ -103,27 +106,54 @@ const generateComment = (comment) => {
     return generateNewCommentString(finalcommentArray);
 }
 
+const copieToClipBoard = () => {
+
+    let range = document.createRange();
+    range.selectNode(resultParagraph);
+    // clear current selection
+    window.getSelection().removeAllRanges();
+    // to select text
+    window.getSelection().addRange(range);
+    document.execCommand("copy");
+    // to deselect
+    window.getSelection().removeAllRanges();
+
+    copyAlert.classList.remove('alert-visible');
+
+    setTimeout(() => {
+        copyAlert.classList.add('alert-visible');
+    }, 3000);
+}
+
+const createComentEventFunction = () => {
+    const comment = textArea.value;
+    const result = generateComment(comment);
+    resultParagraph.innerHTML = result;
+    copieToClipBoard();
+}
+
 // =============================================================================
 
 // =============================================================================
 // ========================== Adding event listeners ===========================
 // =============================================================================
+
+document.addEventListener('keydown', function (event) {
+    event.stopPropagation();  
+    if (event.keyCode === 13) {
+        createComentEventFunction();
+    }
+});
+
 convertBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    const comment = textArea.value;
-    const result = generateComment(comment);
-    resultParagraph.innerHTML = result;
+    event.preventDefault();  
+    event.stopPropagation();   
+    createComentEventFunction();
 });
 
 clearBtn.addEventListener('click', function (event) {
-    event.preventDefault();
+    event.preventDefault();  
+    event.stopPropagation();  
     textArea.value = '';
-    resultParagraph.value = '';
+    resultParagraph.innerHTML = '';
 });
-
-copyBtn.addEventListener('click', function (event) {
-    event.preventDefault();
-    const value = resultParagraph.value;
-
-});
-
